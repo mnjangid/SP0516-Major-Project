657f788b5109ba28f256f2a9170aad84a3539080
Krishan <krishan937611@gmail.com>
2026-02-18 20:16:47 +0530

    added a data file for regional dashboard and updated the sales trend overview

---
+++ regionalOfficerDashboardData.js
@@ -0,0 +1,30 @@
+
+export const labels = ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+export const datasets = [
+    {
+        label: 'Store A',
+        data: [35, 45, 115, 50, 65, 40],
+        backgroundColor: '#5b6abf'
+    },
+    {
+        label: 'Store B',
+        data: [50, 55, 65, 85, 95, 30],
+        backgroundColor: '#e07b7b'
+    },
+    {
+        label: 'Store C',
+        data: [60, 75, 90, 100, 110, 100],
+        backgroundColor: '#5bbfbf'
+    },
+    {
+        label: 'Store D',
+        data: [70, 80, 95, 105, 120, 125],
+        backgroundColor: '#e0b85b'
+    },
+    {
+        label: 'Store E',
+        data: [40, 50, 60, 70, 85, 95],
+        backgroundColor: '#5b8abf'
+    }
+];
